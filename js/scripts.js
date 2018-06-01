3c73b863609cc2acad2aefd4b7c0d4f583779695
Will Swanson <wcs97214@gmail.com>
2018-06-01 13:24:03 -0700

    style text in result, set js to fade in result, fade out form

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
     var cityInput = $("input#city").val();
     var stateInput = $("input#state").val();
     var zipInput = $("input#zip").val();
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+    $(".address2").text(address2Input);
+    $(".city").text(cityInput);
+    $(".state").text(stateInput);
+    $(".zip").text(zipInput);
     var q1i = parseInt($("input:radio[name=q1]:checked").val());
     var q2i = parseInt($("input:radio[name=q2]:checked").val());
     var q3i = parseInt($("input:radio[name=q3]:checked").val());
@@ -15,9 +21,10 @@ $(document).ready(function() {
     if (q1i === 3 || q2i === 3 || q3i === 3 || q4i === 3) {
       $("#ruby").hide();
       $("#css").hide();
-      $("#sharp").show();
-      $("#userData").show();
-      $("#surveyForm").hide();
+      $("#sharp").delay(700).fadeIn(1000);
+      $("#userData").delay(700).fadeIn(4000);
+      $("#surveyForm").fadeOut(500);
+      (document.body.style.backgroundColor = "#AA0000").delay(2000).fadeIn(1000);
     } else if (q1i === 2 && q2i === 2) {
       $("#ruby").hide();
       $("#css").show();
