daa568b8d8900a9c8b6103b6699534736f863810
Will Swanson <wcs97214@gmail.com>
2018-06-01 11:28:48 -0700

    add more text, images, and formatting to results. build decision logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,45 @@
 $(document).ready(function() {
   $("#surveyForm").submit(function(event) {
-    var spiceInput = $("input:radio[name=spice]:checked").val();
-    var ans1 = parseInt($("#q1").val()) + parseInt($("#q2").val()) + parseInt($("#q3").val()) + parseInt($("#q4").val());
-
-    if (ans1 >= 12) {
-      $("#bird").hide();
-      $("#cookie").hide();
-      $("#grouch").show();
-    } else if (ans1 > 8) {
-      $("#bird").hide();
-      $("#cookie").show();
-      $("#grouch").hide();
-    } else if (ans1 >= 4) {
-      $("#bird").show();
-      $("#cookie").hide();
-      $("#grouch").hide();
-    } else {
-      $("#bird").hide();
-      $("#cookie").hide();
-      $("#grouch").hide();
+    var nameInput = $("input#name").val();
+    var addressInput = $("input#address").val();
+    var address2Input = $("input#address2").val();
+    var cityInput = $("input#city").val();
+    var stateInput = $("input#state").val();
+    var zipInput = $("input#zip").val();
+    var q1i = parseInt($("input:radio[name=q1]:checked").val());
+    var q2i = parseInt($("input:radio[name=q2]:checked").val());
+    var q3i = parseInt($("input:radio[name=q3]:checked").val());
+    var q4i = parseInt($("input:radio[name=q4]:checked").val());
+    var ans1 = q1i + q2i + q3i + q4i;
+    // debugger;
+    if (q1i === 3 || q2i === 3 || q3i === 3 || q4i === 3) {
+      $("#ruby").hide();
+      $("#css").hide();
+      $("#sharp").show();
+      $("#userData").show();
+      $("#surveyForm").hide();
+    } else if (q1i === 2 && q2i === 2) {
+      $("#ruby").hide();
+      $("#css").show();
+      $("#sharp").hide();
+      $("#userData").show();
+      $("#surveyForm").hide();
+    } else if (ans1 >= 6) {
+      $("#ruby").hide();
+      $("#css").show();
+      $("#sharp").hide();
+      $("#userData").show();
+      $("#surveyForm").hide();
+    } else if (ans1 < 6) {
+      $("#ruby").show();
+      $("#css").hide();
+      $("#sharp").hide();
+      $("#userData").show();
+      $("#surveyForm").hide();
+    }  else {
+      $("#ruby").hide();
+      $("#css").hide();
+      $("#sharp").hide();
     }
     event.preventDefault();
   });
